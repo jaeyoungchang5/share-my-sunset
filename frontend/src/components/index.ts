2de34a433f705a7f8c3089dd42921bc2e230664e
jaeyoungchang5 <58242673+jaeyoungchang5@users.noreply.github.com>
2021-12-21 17:29:25 -0500

    feed works but no styling

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,2 +1,3 @@
 export * from './Post';
 export * from './Profile';
+export * from './Feed';
--- frontend/src/pages/feed/FeedPage.tsx
+++ frontend/src/pages/feed/FeedPage.tsx
@@ -1,30 +1,89 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, ScrollView, StyleSheet, Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { IUser } from '../../interfaces';
 import { getFeed, getSunsetById } from '../../utils';
+import { Sunset } from '../../components';
+
+interface IFeedItem {
+	_id: string,
+	userId: string,
+	sunsetImage: string,
+	description: string,
+	createdAt: string,
+	updatedAt: string,
+	__v: number
+}
+
+interface IFeed {
+	result: string,
+	message: string,
+	data: IFeedItem[]
+}
 
 export function FeedPage(props: any) {
 	const user: IUser = props.route.params.user;
-	const [feed, setFeed] = useState([]);
+	const [feed, setFeed] = useState<IFeed>();
 
 	useEffect(() => {
 		loadFeed();
 	}, []);
 
 	function loadFeed() {
-		getFeed(user._id)
-		.then(res => {
-			setFeed(res);
-			return;
-		})
+		// getFeed(user._id)
+		// .then(res => {
+		// 	setFeed(res);
+		// 	return;
+		// })
+		const loadedFeeded: IFeed = {
+			"result": "success",
+			"message": "Got feed",
+			"data": [
+				// {
+				// 	"_id": "619ee9eb6eb61ae482f595e2",
+				// 	"userId": "619ee6a0296e594128b0745a",
+				// 	"sunsetImage": "2021-11-25T01:42:03.428ZWorkout.png",
+				// 	"description": "Maureen's workout plan",
+				// 	"createdAt": "2021-11-25T01:42:03.936Z",
+				// 	"updatedAt": "2021-11-25T01:42:03.936Z",
+				// 	"__v": 0
+				// },
+				{
+					"_id": "619ee9c36eb61ae482f595dc",
+					"userId": "619ee65d296e594128b07458",
+					"sunsetImage": "2021-11-25T01:41:20.987ZMe@ND.HEIC",
+					"description": "Jae's first day of school",
+					"createdAt": "2021-11-25T01:41:23.939Z",
+					"updatedAt": "2021-11-25T01:41:23.939Z",
+					"__v": 0
+				},
+				// {
+				// 	"_id": "619ee9666eb61ae482f595ac",
+				// 	"userId": "619ee65d296e594128b07458",
+				// 	"sunsetImage": "2021-11-25T01:39:47.154ZFamily.jpg",
+				// 	"description": "Jae's big family pic",
+				// 	"createdAt": "2021-11-25T01:39:50.148Z",
+				// 	"updatedAt": "2021-11-25T01:39:50.148Z",
+				// 	"__v": 0
+				// }
+			]
+		}
+		setFeed(loadedFeeded);
+	}
+
+	function renderItem({ item } : any) {
+		console.log(item)
+		return (
+			<Sunset />
+		);
 	}
 
 	return (
-		<View style={styles.container}>
-			<Text>Enjoy your feed!</Text>
-			<StatusBar style="auto" />
-		</View>
+		<FlatList style={styles.scroll}
+			data={feed?.data}
+			renderItem={renderItem}
+			keyExtractor={(item) => item._id}
+		/>
 	);
 }
 
@@ -35,4 +94,9 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
 	},
+	scroll: {
+        backgroundColor: '#FFF',
+        flex: 1,
+        overflow: 'hidden'
+    }
 });
