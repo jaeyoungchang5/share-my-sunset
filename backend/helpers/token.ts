7e645c3a0b9a5bb3d151d8e5710e858de5aad872
jaeyoungchang5 <58242673+jaeyoungchang5@users.noreply.github.com>
2021-12-24 13:44:26 -0500

    update token to only keep track of userId

--- backend/helpers/token.ts
+++ backend/helpers/token.ts
@@ -12,9 +12,7 @@ import * as jwt from 'jsonwebtoken';
  */
 export function createToken(user: any) {
     const body = {
-        username: user.username, 
-        firstName: user.firstName, 
-        lastName: user.lastName 
+        userId: user._id
     }
 
     return jwt.sign({ user: { body } }, process.env.ACCESS_TOKEN_SECRET);
